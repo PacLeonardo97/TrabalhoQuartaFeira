6e2a272dc7ed3aca6e739b13e3ffe88bc96bb0de
PacLeonardo97 <56130116+PacLeonardo97@users.noreply.github.com>
2019-10-25 16:51:05 -0300

    Adicionado logo na loadingScreen

--- src/TelaLoadingScreen.java
+++ src/TelaLoadingScreen.java
@@ -1,9 +1,9 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JProgressBar;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.Container;
 
 public class TelaLoadingScreen extends JFrame{
     /**
@@ -16,40 +16,43 @@ public class TelaLoadingScreen extends JFrame{
     }
 
     private JProgressBar progresso;
-
+    private ImageIcon imagem = new ImageIcon(getClass().getResource("logo.jpg"));
+    private JLabel label =new JLabel(imagem);
+   
     public TelaLoadingScreen() {
-        super("Tela de Carregamento");
-        progresso = new JProgressBar();
-        progresso.setStringPainted(true);
+	    super("Tela de Carregamento");
 
-        Container tela = getContentPane();
-        tela.setLayout(new GridLayout(3, 2));
-        JPanel painel1 = new JPanel(new FlowLayout());
-
-        painel1.add(progresso);
-
-        tela.add(painel1);
-
-        new Thread() {
-            public void run() {
-                for (int i = 0; i < 101; i++) {
-                    try {
-                        sleep(10);
-                        progresso.setValue(i);
-
-                    } catch (InterruptedException e) {
-                        
-                        e.printStackTrace();
-                    }
-                }
-                new TelaLogin().setVisible(true); //mudar para tela de 
-                TelaLoadingScreen.this.dispose();
-            }
-        }.start();
-
-        setSize(500,500);
+	    progresso = new JProgressBar();
+	    progresso.setStringPainted(true);
+	    
+	    new Thread() {
+	        public void run() {
+	            for (int i = 0; i < 101; i++) {
+	                try {
+	                    sleep(10);
+	                    progresso.setValue(i);
+	
+	                } catch (InterruptedException e) {
+	                    
+	                    e.printStackTrace();
+	                }
+	            }
+	            new TelaLogin().setVisible(true); //mudar para tela de 
+	            TelaLoadingScreen.this.dispose();
+	        }
+	    }.start();
+	
+	    setSize(387,252);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		label.setBounds(42, 11, 279, 119);
+		getContentPane().add(label);
+		
+		
+		progresso.setBounds(64, 167, 245, 17);
+		getContentPane().add(progresso);
     }
 }
